e05c265e2a5db7e6641425fc7cca1b9ffc7a546d
Marco <marcobuesing@icloud.com>
2024-02-23 02:18:04 +0100

    w

---
+++ arduino/arduino/arduino.ino
@@ -0,0 +1,84 @@
+#include <Servo.h>
+
+
+#define MIN_PULSE_LENGTH 1000  // Minimum pulse length in µs
+#define MAX_PULSE_LENGTH 2000  // Maximum pulse length in µs
+#define MAX_MOTOR_SPEED 1500   // Highest safe motor speed
+
+
+Servo motA;
+char data;
+
+////////
+
+
+/// NIEMALS ÜBER 50% LEISTUNG PRO MOTOR!!!!!!!!
+
+
+////////
+
+
+void setup() {
+    Serial.begin(9600);
+    
+    motA.attach(3, MIN_PULSE_LENGTH, MAX_PULSE_LENGTH);
+
+    
+    displayInstructions();
+}
+
+void loop() {
+    if (Serial.available()) {
+        data = Serial.read();
+        switch (data) {
+            case 48 : Serial.println("Sending minimum throttle");
+                      motA.writeMicroseconds(MIN_PULSE_LENGTH);
+            break;
+
+            case 49 : Serial.println("Sending maximum throttle");
+                      motA.writeMicroseconds(MAX_PULSE_LENGTH);
+            break;
+
+            case 50 : Serial.print("Running test in 3");
+                      delay(1000);
+                      Serial.print(" 2");
+                      delay(1000);
+                      Serial.println(" 1...");
+                      delay(1000);
+                      test();
+            break;
+            case 51:
+              Serial.println("Sending 1200");
+              motA.writeMicroseconds(1200);
+              break;
+            case 52:
+              Serial.println("Sending 1050");
+              motA.writeMicroseconds(1050);
+            
+        }
+    }
+}
+
+void test()
+{
+    for (int i = MIN_PULSE_LENGTH; i <= MAX_MOTOR_SPEED; i += 1) {
+        Serial.print("Pulse length = ");
+        Serial.println(i);
+        
+        motA.writeMicroseconds(i);
+        
+        delay(15);
+    }
+
+    Serial.println("STOP");
+    motA.writeMicroseconds(MIN_PULSE_LENGTH);
+    
+}
+
+void displayInstructions()
+{  
+    Serial.println("READY - PLEASE SEND INSTRUCTIONS AS FOLLOWING :");
+    Serial.println("\t0 : Send min throttle");
+    Serial.println("\t1 : Send max throttle");
+    Serial.println("\t2 : Run test function\n");
+}
