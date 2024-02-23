6d93b612fff6c8c0c0f58b397101cecf70d89718
Marco <marcobuesing@icloud.com>
2024-02-23 22:25:38 +0100

    w

--- arduino/read_pwm_and_print/read_pwm_and_print.ino
+++ arduino/read_pwm_and_print/read_pwm_and_print.ino
@@ -1,9 +1,21 @@
 #include <Servo.h>
 
-const int pwmPin1 = 2;
-const int pwmPin2 = 4;
-const int pwmPin3 = 7;
-const int pwmPin4 = 8;
+const int signalinfrompi1 = 2;
+const int signalinfrompi2 = 4;
+const int signalinfrompi3 = 7;
+const int signalinfrompi4 = 8;
+
+int infrompihigh1 = 0;
+int infrompihigh2 = 0;
+int infrompihigh3 = 0;
+int infrompihigh4 = 0;
+
+int infrompilow1 = 0;
+int infrompilow2 = 0;
+int infrompilow3 = 0;
+int infrompilow4 = 0;
+
+
 
 const int ESC_PIN_1 = 3;
 const int ESC_PIN_2 = 5;
@@ -13,11 +25,6 @@ const int ESC_PIN_4 = 9;
 const int THROTTLE_MIN = 1000;
 const int THROTTLE_FULL_MAX = 2000;
 const int THROTTLE_MAX = 1500;
-
-int dutyCycle1 = 0;
-int dutyCycle2 = 0;
-int dutyCycle3 = 0;
-int dutyCycle4 = 0;
 
 int pitopwm1 = 0;
 int pitopwm2 = 0;
@@ -46,59 +53,65 @@ void setup() {
   delay(2000); // Wait for 2 seconds
 
   
-  Serial.begin(9600);
+  Serial.begin(10000);
 }
 
 void loop() {
-  unsigned long pulseWidth1 = pulseIn(pwmPin1, HIGH);
-  int dutyCycle1 = map(pulseWidth1, 0, 10000, 0, 100);
-  
-  unsigned long pulseWidth2 = pulseIn(pwmPin2, HIGH);
-  int dutyCycle2 = map(pulseWidth2, 0, 10000, 0, 100);
-  
-  unsigned long pulseWidth3 = pulseIn(pwmPin3, HIGH);
-  int dutyCycle3 = map(pulseWidth3, 0, 10000, 0, 100);
-  
-  unsigned long pulseWidth4 = pulseIn(pwmPin4, HIGH);
-  int dutyCycle4 = map(pulseWidth4, 0, 10000, 0, 100);
 
-  // // Print the pulse width and duty cycle
-  // Serial.print("Pulse Width 1 (microseconds): ");
-  // Serial.print(pulseWidth1);
-  // Serial.print("    Duty Cycle (%): ");
-  // Serial.println(dutyCycle1);
+  // int infrompi1 = pulseIn(signalinfrompi1, HIGH, 50000UL);
+  // int infrompi2 = pulseIn(signalinfrompi2, HIGH, 50000UL);
+  // int infrompi3 = pulseIn(signalinfrompi3, HIGH, 50000UL);
+  // int infrompi4 = pulseIn(signalinfrompi4, HIGH, 50000UL);
 
-  // Serial.print("Pulse Width 2 (microseconds): ");
-  // Serial.print(pulseWidth2);
-  // Serial.print("    Duty Cycle (%): ");
-  // Serial.println(dutyCycle2);
-
-  // Serial.print("Pulse Width 3 (microseconds): ");
-  // Serial.print(pulseWidth3);
-  // Serial.print("    Duty Cycle (%): ");
-  // Serial.println(dutyCycle3);
-
-  // Serial.print("Pulse Width 4 (microseconds): ");
-  // Serial.print(pulseWidth4);
-  // Serial.print("    Duty Cycle (%): ");
-  // Serial.println(dutyCycle4);
-
-
-  int pitopwm1 = map(dutyCycle1, 0, 100, 1000, 1300);
-  int pitopwm2 = map(dutyCycle2, 0, 100, 1000, 1300);
-  int pitopwm3 = map(dutyCycle3, 0, 100, 1000, 1300);
-  int pitopwm4 = map(dutyCycle4, 0, 100, 1000, 1300);
+  // int infrompi1 = pulseIn(signalinfrompi1, LOW, 50000UL);
+  // int infrompi2 = pulseIn(signalinfrompi2, LOW, 50000UL);
+  // int infrompi3 = pulseIn(signalinfrompi3, LOW, 50000UL);
+  // int infrompi4 = pulseIn(signalinfrompi4, LOW, 50000UL);
 
 
 
-  esc1.writeMicroseconds(pitopwm1);
-  esc2.writeMicroseconds(pitopwm2);
-  esc3.writeMicroseconds(pitopwm3);
-  esc4.writeMicroseconds(pitopwm4);
 
 
 
-  // delay(20);  // Delay for stability, adjust as needed
+  // pitopwm1 = map(infrompi1, 0, 10000, 1000, 1300);
+  // pitopwm1 = map(infrompi2, 0, 10000, 1000, 1300);
+  // pitopwm1 = map(infrompi3, 0, 10000, 1000, 1300);
+  // pitopwm1 = map(infrompi4, 0, 10000, 1000, 1300);
+
+
+  pitopwm1 = infrompi(signalinfrompi1);
+  pitopwm2 = infrompi(signalinfrompi2);
+  pitopwm3 = infrompi(signalinfrompi3);
+  pitopwm4 = infrompi(signalinfrompi4);
+
+  infrompihigh1 = map(pitopwm1, 0, 100, 1000, 1300);
+  infrompihigh2 = map(pitopwm2, 0, 100, 1000, 1300);
+  infrompihigh3 = map(pitopwm3, 0, 100, 1000, 1300);
+  infrompihigh4 = map(pitopwm4, 0, 100, 1000, 1300);
+
+  esc1.writeMicroseconds(infrompihigh1);
+  esc2.writeMicroseconds(infrompihigh2);
+  esc3.writeMicroseconds(infrompihigh3);
+  esc4.writeMicroseconds(infrompihigh4);
+
+
 }
+
+
+byte infrompi(byte pin)
+{
+  unsigned long highTime = pulseIn(pin, HIGH, 300UL);
+  unsigned long lowTime = pulseIn(pin, LOW, 300UL);
+
+  if (highTime == 0 || lowTime == 0)
+    return digitalRead(pin) ? 100 : 0;
+
+  return (100 * highTime) / (highTime + lowTime);
+}
+
+
+
+
+
 
 
